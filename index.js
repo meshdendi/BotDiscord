b1a14a41aeb53a31388c097d81b818c93b5c3267
meshdendi <soubra.abed@gmail.com>
2020-05-08 16:08:17 +0300

    make it better

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const discord = require("discord.js");
 const bot = new discord.Client();
 const request = require("request");
 const config = require("./config.json");
+const fs = require('fs');
 
 var bullet_position = Math.floor(Math.random() * 7) + 1;
 
@@ -14,10 +15,10 @@ bot.on("message", (msg) => {
 
   if (args.length == 1) {
     if (args[0].toLowerCase() == "yomama") {
-      request("https://api.yomomma.info", (err, res, body) => {
-        if (!err && res.statusCode == 200) {
-          msg.channel.send(JSON.parse(body).joke);
-        }
+      fs.readFile('./assets/text/yomama.txt', (err, data) => {
+        if (err) throw err;
+        var arr = data.toString().split('\n');
+        msg.channel.send(arr[Math.floor(Math.random() * arr.length) + 1]);
       });
     } else if (args[0].toLowerCase() == "!shoot") {
       if (Math.floor(Math.random() * 7) + 1 == bullet_position) {
